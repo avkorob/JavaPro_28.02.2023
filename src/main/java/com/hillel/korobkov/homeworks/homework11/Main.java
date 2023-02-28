af286e0070798d475aadf847fd0ee7de1e46f691
Korobkov <av.korob@gmail.com>
2023-02-28 22:10:02 +0200

    Init commit

---
+++ src/main/java/com/hillel/korobkov/homeworks/homework11/Main.java
@@ -0,0 +1,23 @@
+package com.hillel.korobkov.homeworks.homework11;
+
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        final int counter = 8;
+        int[] team1 = new int[counter];
+        int[] team2 = new int[counter];
+        double sum1 = 0;
+        double sum2 = 0;
+
+        for (int i = 0; i < counter; i++) {
+            team1[i] = (int) (18 + (Math.random() * 23));
+            team2[i] = (int) (18 + (Math.random() * 23));
+            sum1 += team1[i];
+            sum2 += team2[i];
+        }
+        System.out.println(Arrays.toString(team1));
+        System.out.println(Arrays.toString(team2));
+        System.out.println("середній вік гравців першої команди  : " + (sum1 / counter));
+        System.out.print("середній вік гравців другої команди  : " + (sum2 / counter));
+    }
+}
