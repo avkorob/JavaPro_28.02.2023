507a7fcd536f72d97c8ebae8a2243481563b8ea6
Korobkov <av.korob@gmail.com>
2023-03-08 15:36:41 +0200

    homework-3

--- src/main/java/com/hillel/korobkov/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/korobkov/homeworks/homework3/Main.java
@@ -1,56 +1,41 @@
 package com.hillel.korobkov.homeworks.homework3;
 
-import java.util.Map;
-import java.util.HashMap;
-//         Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-//        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-//        typically using all the original letters exactly once.
-//          Example 1:
-//
-//        Input: s = "anagram", t = "nagaram"
-//        Output: true
-//        Example 2:
-//
-//        Input: s = "rat", t = "car"
-//        Output: false
+import java.util.Arrays;
 
 public class Main {
-
-    public static boolean isAnagram(String str1, String str2) {
-
-        if (str1 == null || str2 == null) {
-            return false;
-        }
-        if (str1.length() != str2.length()) {
-            return false;
-        }
-        Map<Character, Integer> freq = new HashMap<>();
-        for (char c : str1.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-        }
-        for (char c : str2.toCharArray()) {
-            if (!freq.containsKey(c)) {
-                return false;
-            }
-            freq.put(c, freq.get(c) - 1);
-            if (freq.get(c) == 0) {
-                freq.remove(c);
-            }
-        }
-        return freq.isEmpty();
-    }
-
     public static void main(String[] args) {
-        String str1 = "middle";
-        String str2 = "midle";
+        String str1;
+        String str2;
 
-        if (isAnagram(str1, str2)) {
-            System.out.print("Anagram");
-        } else {
-            System.out.print("Not an Anagram");
+        str1 = "Show must go on";
+        str2 = "On go must wosh";
+        System.out.println("Input1 - " + str1);
+        System.out.println("Input2 - " + str2);
+
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        if (arr1.length != arr2.length) {
+            System.out.println("Not an anagramm");
+            System.exit(0);
         }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        boolean a = Arrays.equals(arr2, arr1);
+
+//        for (int i = 0; i < arr1.length; i++) {
+//            if (arr1[i] != arr2[i]){
+//                System.out.println("Not an anagramm");
+//                System.exit(0);
+//            }
+//        }
+        System.out.println(a);
     }
-
-
 }
+
+
+
